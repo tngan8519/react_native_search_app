1b5f7874c74eb5a6766084fb087a53e4d6ddb20d
tngan8519 <71539113+tngan8519@users.noreply.github.com>
2020-12-28 02:15:04 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
 import Header from "./components/Header";
 import Content from "./components/Content";
 
 export default function App() {
   return (
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
     <View>
       <Header />
       <Content />
